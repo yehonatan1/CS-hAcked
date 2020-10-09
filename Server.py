ec35a72c691a57e94fd072b63c921dd99f7f195c
yehonatan1 <54950358+yehonatan1@users.noreply.github.com>
2020-10-09 15:24:46 -0700

    Update Server.py

--- Server.py
+++ Server.py
@@ -58,24 +58,36 @@ def main():
     data = s.recv(1024)
     print(data.decode())
 
+    # for counting how many words did i send to the server
     i = 0
+    response = s.recv(1024)
+    print('response is ' + str(response))
 
+    # looping for each possible sequence
     for option in options:
         words = option.split(' ')
         for word in words:
             i += 1
+
+            # adding \n for each word
             word += "\n"
             print(word)
+
+            # encrypting each word and sending to the server
             arc4 = ARC4('csa-mitm-key')
             cipher = arc4.encrypt(word)
             s.send(cipher)
 
         response = s.recv(4096)
-        if response != b'\x01:\xa12$\xc1O,A\x82\xee\x08}\x80\x1f\x10T\xc9\x92\xa5_\x1b\xec@\xf3\xdb;\x952\xea8\xf9' and response != b'Welcome! your RC4 key is: csa-mitm-key\n':
+
+        # checking if the response is not some crap
+        if response != b'\x01:\xa12$\xc1O,A\x82\xee\x08}\x80\x1f\x10T\xc9\x92\xa5_\x1b\xec@\xf3\xdb;\x952\xea8\xf9' and response != b'Welcome! your RC4 key is: csa-mitm-key\n' and response != b'\xee\x08}\x80\x1f\x10T\xc9\x92\xa5_\x1b\xec@\xf3\xdb;\x952\xea8\xf9':
             print(i)
             print(response)
+            print(arc4.decrypt(response))
             return response
 
+        # creating a new connection to the server
         s = socket.socket()
         s.connect(("3.126.154.76", 80))
 

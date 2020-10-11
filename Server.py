a352dff7f106db508ea05ce25b52be173000a39f
yehonatan1 <54950358+yehonatan1@users.noreply.github.com>
2020-10-11 02:42:21 -0700

    Update Server.py

--- Server.py
+++ Server.py
@@ -51,43 +51,37 @@ options = ['organization administration I as I environmental about accept abilit
            'particularly administration a be a environmental about across ability according']
 
 
-def main():
-    # for counting how many words did i send to the server
-    i = 0
+def main(option):
+    arc4 = ARC4('csa-mitm-key')
 
     # looping for each possible sequence
-    for option in options:
-        # creating a new connection to the server
-        s = socket.socket()
-        s.connect(("3.126.154.76", 80))
-        data = s.recv(1024)
-        data1 = s.recv(1024)
+    s = socket.socket()
+    s.connect(("3.126.154.76", 80))
+    data = s.recv(1024)
+    data1 = s.recv(1024)
 
-        words = option.split(' ')
-        for word in words:
-            i += 1
+    print('data1 is ' + str(arc4.decrypt(data1)))
 
-            # adding \n for each word
-            word += "\n"
-            print(word)
+    words = option.split(' ')
+    for word in words:
+        # adding \n for each word
+        word += "\n"
 
-            # encrypting each word and sending to the server
-            arc4 = ARC4('csa-mitm-key')
-            cipher = arc4.encrypt(word)
-            s.send(cipher)
+        # encrypting each word and sending to the server
+        cipher = arc4.encrypt(word)
+        s.send(cipher)
 
-        response = s.recv(1024)
-        print(response)
+    response = s.recv(1024)
 
-        # checking if the response is not some crap
-        if response != b'\x01:\xa12$\xc1O,A\x82\xee\x08}\x80\x1f\x10T\xc9\x92\xa5_\x1b\xec@\xf3\xdb;\x952\xea8\xf9' and response != b'Welcome! your RC4 key is: csa-mitm-key\n' and response != b'\xee\x08}\x80\x1f\x10T\xc9\x92\xa5_\x1b\xec@\xf3\xdb;\x952\xea8\xf9':
-            print(i)
-            print(response)
-            print(arc4.decrypt(response))
-            return response
-
-    print(i)
+    # checking if the response is not some crap
+    if response != b'\x01:\xa12$\xc1O,A\x82\xee\x08}\x80\x1f\x10T\xc9\x92\xa5_\x1b\xec@\xf3\xdb;\x952\xea8\xf9' and response != b'Welcome! your RC4 key is: csa-mitm-key\n' and response != b'\xee\x08}\x80\x1f\x10T\xc9\x92\xa5_\x1b\xec@\xf3\xdb;\x952\xea8\xf9':
+        message = arc4.decrypt(response)
+        s.close()
+        if message.startswith(b'CSA'):
+            print('The flag is ' + str(message))
+            quit(1)
 
 
 if __name__ == "__main__":
-    main()
+    for option in options:
+        main(option)
